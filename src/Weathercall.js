fca6ccf0f2b046fff293c57187b913c19d136047
AbdulHadi1010 <hadi2408466@gmail.com>
2023-04-16 01:23:40 +0500

    Update Weathercall.js
    adding api data into text

--- src/Weathercall.js
+++ src/Weathercall.js
@@ -1,26 +1,66 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
+import axios from "axios";
 
 export default function Weathercall() {
-    // const axios = require('axios');
+  // const [apiData, setApiData] = useState();
+  const [loading, setLoading] = useState(true);
+  // const [tempinC, settempinC] = useState(true);
+  const tempchangertoC = (num) => {
+    return Math.round(num-273.15)   
+  }
+  const apiData = {
+    coord: {lon: 74.32,lat: 31.58,},
+    weather: [{id: 721,main: "Haze",description: "haze",icon: "50n",},],
+    base: "stations",
+    main: {temp: 301.08,feels_like: 300.51,temp_min: 299.15,temp_max: 301.08,pressure: 1007,humidity: 36,},
+    visibility: 5000,
+    wind: {speed: 3.09,deg: 90,},
+    clouds: {all: 100,},
+    dt: 1681585723,
+    sys: {type: 1,id: 7585,country: "PK",sunrise: 1681605223,sunset: 1681651870,},
+    timezone: 18000,
+    id: 1172451,
+    name: "Lahore",
+    cod: 200,
+  };
+  // const callApi = () => {
+  // let config = {
+  //   method: "get",
+  //   maxBodyLength: Infinity,
+  //   url: "https://api.openweathermap.org/data/2.5/weather?lat=31.58&lon=74.32&appid=134bcc17ff7fbd17a3ec89f642825260",
+  //   headers: {},
+  // };
 
-let config = {
-  method: 'get',
-  maxBodyLength: Infinity,
-  url: 'https://api.openweathermap.org/data/2.5/weather?lat=31.58&lon=74.32&appid=134bcc17ff7fbd17a3ec89f642825260',
-  headers: { }
-};
-
-axios.request(config)
-.then((response) => {
-  console.log(JSON.stringify(response.data));
-})
-.catch((error) => {
-  console.log(error);
-});
+  // axios
+  //   .request(config)
+  //   .then((response) => {
+  //     setApiData(response.data);
+  //     setLoading(false);
+  //     console.log(JSON.stringify(response.data));
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+  // }
+  // useEffect(() => {
+  //   callApi();
+  // }, []);
   return (
-    
-        <Text>This is the API call file</Text>
-  )
+    <View>
+      {/* {loading ? (
+        <ActivityIndicator size="large" color="#00ff00" />
+      ) : ( */}
+        <>
+          <Text style={{ marginTop: 10, fontSize: 15, textAlign: "center" }}>
+            City: {apiData.name}
+          </Text>
+          <Text style={{ marginTop: 10, fontSize: 15, textAlign: "center" }}>Temparature: {tempchangertoC(apiData?.main.temp)}° C</Text>
+          <Text style={{ marginTop: 10, fontSize: 15, textAlign: "center" }}>Feels Like: {tempchangertoC(apiData?.main.feels_like)}° C</Text>
+          <Text style={{ marginTop: 10, fontSize: 15, textAlign: "center" }}>Max temparature: {tempchangertoC(apiData?.main.temp_max)}° C</Text>
+          <Text style={{ marginTop: 10, fontSize: 15, textAlign: "center" }}>Min temparature: {tempchangertoC(apiData?.main.temp_min)}° C</Text>
+        </>
+      {/* )} */}
+    </View>
+  );
 }
