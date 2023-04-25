5abf87021dbf39f0a3572dafba5ab87b5fde7b3f
AbdulHadi1010 <hadi2408466@gmail.com>
2023-04-25 23:36:25 +0500

    Gps check

--- android/app/src/main/java/com/weatherapp/MainApplication.java
+++ android/app/src/main/java/com/weatherapp/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+import com.heanoria.library.reactnative.locationenabler.RNAndroidLocationEnablerPackage;
 
 import expo.modules.ApplicationLifecycleDispatcher;
 import expo.modules.ReactNativeHostWrapper;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
       protected List<ReactPackage> getPackages() {
         @SuppressWarnings("UnnecessaryLocalVariable")
         List<ReactPackage> packages = new PackageList(this).getPackages();
+        new RNAndroidLocationEnablerPackage();
         // Packages that cannot be autolinked yet can be added manually here, for example:
         // packages.add(new MyReactNativePackage());
         return packages;
--- src/Weathercall.js
+++ src/Weathercall.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { ActivityIndicator, Text, TouchableOpacity, View } from "react-native";
 import axios from "axios";
 import GetLocation from "react-native-get-location";
+import RNAndroidLocationEnabler from 'react-native-android-location-enabler';
 
 export default function Weathercall() {
   const [loc, setloc] = useState ("");
@@ -10,45 +11,67 @@ export default function Weathercall() {
   const tempchangertoC = (num) => {
     return Math.round(num-273.15)   
   }
-  const Locationfetch = () => {
-    GetLocation.getCurrentPosition({
-      enableHighAccuracy: true,
-      timeout: 60000,
-  })
-  .then(location => {
-      setloc(location)
-      console.log("Initial location: ", loc);
-      
-  })
-  .catch(error => {
-      const { code, message } = error;
-      console.warn(code, message);
-  })}
-  const callApi = () => {
-  let config = {
-    method: "get",
-    maxBodyLength: Infinity,
-    url: `https://api.openweathermap.org/data/2.5/weather?lat=${loc?.latitude}&lon=${loc?.longitude}&appid=134bcc17ff7fbd17a3ec89f642825260`,
-    headers: {},
-  };
-  axios
-    .request(config)
-    .then((response) => {
-      setApiData(response.data);
-      setLoading(false);
-      console.log(JSON.stringify(response.data));
+  const UpdateLocation = () => {
+    RNAndroidLocationEnabler.promptForEnableLocationIfNeeded({
+      interval: 10000,
+      fastInterval: 5000,
     })
-    .catch((error) => {
-      console.log(error);
+    .then((data) => {    
+      Locationfetch()
+  })
+    .catch((err) => {
+      // The user has not accepted to enable the location services or something went wrong during the process
+      // "err" : { "code" : "ERR00|ERR01|ERR02|ERR03", "message" : "message"}
+      // codes :
+      //  - ERR00 : The user has clicked on Cancel button in the popup
+      //  - ERR01 : If the Settings change are unavailable
+      //  - ERR02 : If the popup has failed to open
+      //  - ERR03 : Internal error
     });
+
+    const Locationfetch = () => {
+      GetLocation.getCurrentPosition({
+        enableHighAccuracy: true,
+        timeout: 60000,
+    })
+    .then(location => {
+        // setloc(location)
+        callApi(location)
+        console.log("Initial location: ", location);
+        // console.log("State location: ", loc);
+        
+    })
+    .catch(error => {
+        const { code, message } = error;
+        console.warn(code, message);
+    })}
   }
+  const callApi = (location) => {
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: `https://api.openweathermap.org/data/2.5/weather?lat=${location?.latitude}&lon=${location?.longitude}&appid=134bcc17ff7fbd17a3ec89f642825260`,
+      headers: {},
+    };
+    axios
+      .request(config)
+      .then((response) => {
+        setApiData(response.data);
+        setLoading(false);
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+
   useEffect(() => {
-    Locationfetch()
-    callApi()
+    UpdateLocation()
     }, []);
 
   return (
     <View>
+     {/* {console.log("State Location: ", loc)} */}
       {loading ? (
         <ActivityIndicator size="large" color="#00ff00" />
       ) : (
